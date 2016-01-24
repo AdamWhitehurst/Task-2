d4d111c880bd66004aeb55aa5cb4b466db7cadcf
Adam Whitehurst <Adam.Whitehurst@live.com>
2016-01-23 17:12:41 -0900

    Added method Greetings()

--- Task 2/Task 2.cpp
+++ Task 2/Task 2.cpp
@@ -1,23 +1,54 @@
 #include <iostream>
 #include <fstream>
-using namespace.std;
+#include <time.h>
+
+using namespace std;
 
 class SuperMarket
 {
 private:
-	void greetings();
-	void menu();
-	void printInvoice();
-	void readDatabase();
-	void print_receipt();
-	double bill();
-	double applePrice = 2.99, breadLoafPrice = 3.95, juicePrice = 5.99, butterPrice = 6.99, eggPrice = 4.99;
+	void Greetings(); // Takes user input for the customer name then greets the customer according to the current system time
+	void Menu();
+	void PrintInvoice();
+	void ReadDatabase();
+	void PrintReceipt();
+	char customerName[20];
+	double Bill();
+	double  applePrice = 2.99;
+	double  breadLoafPrice = 3.95;
+	double  juicePrice = 5.99;
+	double  butterPrice = 6.99;
+	double  eggPrice = 4.99;
 public:
-	SuperMarket();
-
+	SuperMarket() {
+		Greetings();
+	}
 };
 
-int main()
+void SuperMarket::Greetings() {
+
+	time_t TheTime; // declare a time object
+	time(&TheTime); // Get the current time
+	struct tm * timeinfo; // This will hold the time in an easy-to-use structure
+	timeinfo = localtime(&TheTime); // This converts from raw time to the structure.
+
+	cout << "Hello there valued customer! May I get your name? ";
+	cin.getline(this->customerName, 20);
+	if (timeinfo->tm_hour < 12) {
+		cout << "Good morning ";
+	}
+	else if (timeinfo->tm_hour < 17) {
+		cout << "Good afternoon ";
+	}
+	else {
+		cout << "Good evening ";
+	}
+	cout << this->customerName << "!" << endl;
+};
+
+int main(void)
 {
+	SuperMarket superMarket;
+
 	return 0;
 }
